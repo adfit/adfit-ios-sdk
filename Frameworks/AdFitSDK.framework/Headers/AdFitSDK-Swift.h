2b02a2ee1b804afd3df4b1c5bc94c4edc7c451b1
kyle.kao <kyle.kao@kakaocorp.com>
2020-12-09 09:29:56 +0900

    3.7.0 업데이트

--- Frameworks/AdFitSDK.framework/Headers/AdFitSDK-Swift.h
+++ Frameworks/AdFitSDK.framework/Headers/AdFitSDK-Swift.h
@@ -188,6 +188,7 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #if __has_warning("-Watimport-in-framework-header")
 #pragma clang diagnostic ignored "-Watimport-in-framework-header"
 #endif
+@import AVKit;
 @import CoreGraphics;
 @import ObjectiveC;
 @import UIKit;
@@ -207,6 +208,11 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 # pragma clang attribute push(__attribute__((external_source_symbol(language="Swift", defined_in="AdFitSDK",generated_declaration))), apply_to=any(function,enum,objc_interface,objc_category,objc_protocol))
 # pragma pop_macro("any")
 #endif
+
+
+@interface AVPlayerViewController (SWIFT_EXTENSION(AdFitSDK))
+@property (nonatomic, readonly) BOOL prefersStatusBarHidden;
+@end
 
 enum AdFitVideoPlayPolicy : NSInteger;
 
