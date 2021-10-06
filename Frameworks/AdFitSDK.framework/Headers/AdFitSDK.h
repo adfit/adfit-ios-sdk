834f0272280c0d0eb2cad65c834ea4d98de92b7f
kyle.kao <kyle.kao@kakaocorp.com>
2021-10-06 12:15:21 +0900

    3.11.1 업데이트

---
+++ Frameworks/AdFitSDK.framework/Headers/AdFitSDK.h
@@ -0,0 +1,20 @@
+//
+//  AdFitSDK.h
+//  AdFitSDK
+//
+//  Created by aiden.gil on 03/07/2017.
+//  Copyright © 2017 Kakao Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "VitalMonitor.h"
+
+//! Project version number for AdFitSDK.
+FOUNDATION_EXPORT double AdFitSDKVersionNumber;
+
+//! Project version string for AdFitSDK.
+FOUNDATION_EXPORT const unsigned char AdFitSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdFitSDK/PublicHeader.h>
+
+
